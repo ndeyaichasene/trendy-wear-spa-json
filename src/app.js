7b0d86837c0ad9951ede1f239acd3a4d964d44c3
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-06-19 02:44:07 +0000

    responsive

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import { initRouter } from './router.js';
-initRouter();
-
+import { header, initHeader } from './components/Header.js';
 import './auth.js';
+
+// Injecte le header une seule fois
+document.getElementById('header-root').innerHTML = header;
+initHeader();
+
+initRouter();
